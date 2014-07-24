3861d111d330619a0e4e18a9b35f98b683e8a44b
urik9 <uri@wix.com>
2014-07-24 15:03:26 +0300

    removed orr's stuff

--- src/test/scala/com/example/JsonParserTest.scala
+++ src/test/scala/com/example/JsonParserTest.scala
@@ -24,24 +24,37 @@ class JsonParserTest extends SpecificationWithJUnit {
     "generate two fields and nums" in new Context {
       Parser.parseJsonObject("{a:3,b:4}") must_== JsonObject(Map("a" -> JsonNumber(3), "b" -> JsonNumber(4)))
     }
-    "gerate a string and and a num" in new Context{
-      Parser.parseJsonObject("{a:3,b:\"four\"}") must_== JsonObject(Map("a" -> JsonNumber(3),"b" -> JsonString("four")))
+    "generate a string and and a num" in new Context {
+      Parser.parseJsonObject("{a:3,b:\"four\"}") must_== JsonObject(Map("a" -> JsonNumber(3), "b" -> JsonString("four")))
     }
-    "gerate a num and and a boolean true" in new Context{
-      Parser.parseJsonObject("{a:3,b:true}") must_== JsonObject(Map("a" -> JsonNumber(3),"b" -> JsonBoolean(true)))
+    "generate a num and and a boolean true" in new Context {
+      Parser.parseJsonObject("{a:3,b:true}") must_== JsonObject(Map("a" -> JsonNumber(3), "b" -> JsonBoolean(true)))
     }
-    "gerate a num and and a boolean true" in new Context{
-      Parser.parseJsonObject("{a:3,b:false}") must_== JsonObject(Map("a" -> JsonNumber(3),"b" -> JsonBoolean(false)))
+    "generate a num and and a Boolean true" in new Context {
+      Parser.parseJsonObject("{a:3,b:false}") must_== JsonObject(Map("a" -> JsonNumber(3), "b" -> JsonBoolean(false)))
     }
-    "generate an empty array" in new Context{
-      Parser.parseJsonObject("{a:[]}") must_==JsonObject(Map("a" -> JsonArray()))
+    "generate an empty array" in new Context {
+      Parser.parseJsonObject("{a:[]}") must_== JsonObject(Map("a" -> JsonArray()))
     }
-    "generate an array with one item" in new Context{
-      Parser.parseJsonObject("{a:[\"b\"]}") must_==JsonObject(Map("a" -> JsonArray((JsonString("b")))))
+    "generate an array with one string item" in new Context {
+      Parser.parseJsonObject("{a:[\"b\"]}") must_== JsonObject(Map("a" -> JsonArray(JsonString("b"))))
     }
-//    "generate a json object inside the json object" in new Context {
-//      Parser.parse("{a:{b:1}}") must_== JsonObject(Map("a" -> JsonObject(Map("b" -> JsonNumber(1)))))
-//    }
+    "generate an array with one int item" in new Context {
+      Parser.parseJsonObject("{a:[3]}") must_== JsonObject(Map("a" -> JsonArray(JsonNumber(3))))
+    }
+    "generate an array with one Boolean item" in new Context {
+      Parser.parseJsonObject("{a:[false]}") must_== JsonObject(Map("a" -> JsonArray(JsonBoolean(false))))
+    }
+    "generate an empty json object inside the json object" in new Context {
+      Parser.parseJsonObject("{a:{}}") must_== JsonObject(Map("a" -> JsonObject(Map.empty)))
+    }
+    "generate an empty json object inside the json object" in new Context {
+      Parser.parseJsonObject("{a:{b:5}}") must_== JsonObject(Map("a" -> JsonObject(Map("b" -> JsonNumber(5)))))
+    }
+
+    //    "generate an array with one int one and string items" in new Context {
+    //      Parser.parseJsonObject("{a:[6, \"f\"]}") must_== JsonObject(Map("a" -> JsonArray(JsonNumber(6),JsonString("f"))))
+    // }
   }
 
 
